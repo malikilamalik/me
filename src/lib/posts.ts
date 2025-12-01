1f327ced2b6d5eedd130a4c0582c021f84b659d7
malikilamalik <penguintheshore@gmail.com>
2025-12-01 15:16:44 +0700

    feat: add json wrangler

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,15 +1,15 @@
 export const get_posts = async () => {
 	// Fetch posts from local Markdown files
-const posts: Post[] = await Promise.all(
-	Object.entries(import.meta.glob('../posts/**/*.md')).map(
-			async ([path, resolver]) => {
-				const resolved = (await resolver()) as { metadata: Post };
-				const { metadata } = resolved;
-				const slug = path.split('/').pop()?.slice(0, -3) ?? '';
-				return { ...metadata, slug };
-			},
-		),
-	);
+	const posts: Post[] = await Promise.all(
+		Object.entries(import.meta.glob('../posts/**/*.md')).map(
+				async ([path, resolver]) => {
+					const resolved = (await resolver()) as { metadata: Post };
+					const { metadata } = resolved;
+					const slug = path.split('/').pop()?.slice(0, -3) ?? '';
+					return { ...metadata, slug };
+				},
+			),
+		);
 
 	let sorted_posts = posts.sort(
 		(a, b) => +new Date(b.date) - +new Date(a.date),
